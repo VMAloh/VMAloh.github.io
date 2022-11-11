47a3ef02388e0e55155e320f47dab745ca65cc79
Apetrei_Roxana_Loredana <loredana.roxy@yahoo.com>
2022-11-11 20:52:45 +0200

    first commit

---
+++ Home/home.js
@@ -0,0 +1,12 @@
+document.getElementById("game1").onclick = function () {
+    location.href = "Game1/game1.html";
+};
+document.getElementById("game2").onclick = function () {
+    location.href = "Game2/game2.html";
+};
+document.getElementById("game3").onclick = function () {
+    location.href = "Game3/game3.html";
+};
+document.getElementById("game4").onclick = function () {
+    location.href = "Game4/game4.html";
+};
