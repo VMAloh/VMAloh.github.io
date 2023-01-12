dfc9c3a32aa7a5b80b6369cec829242126a53ce7
Bria <balacibrianna@gmail.com>
2023-01-12 18:18:15 +0200

    final refact

--- Home/Game2/script2.js
+++ Home/Game2/script2.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
             }
             startButton.removeEventListener("click",startGame)
             nextButton.removeEventListener("click", nextQuestion)
+            home1.removeEventListener("click",goToHome)
+            home2.removeEventListener("click",goToHome)
         }).on('ended', function () {
         var answers = document.getElementsByClassName('btn-answer');
         var numAnswers = answers.length;
@@ -18,6 +20,8 @@ $(document).ready(function () {
         }
             startButton.addEventListener("click",startGame)
             nextButton.addEventListener("click",nextQuestion)
+            home1.addEventListener('click',goToHome)
+            home2.addEventListener('click',goToHome)
     });
     audioIntro.play();
 });
